6ea3198bd041d008398af77a4a33f45b3e68e9df
DhruvPotdar <dhruv.potdar@atimotors.com>
2026-02-09 12:50:30 +0530

    kitty: improvements

--- .config/kitty/tab_bar.py
+++ .config/kitty/tab_bar.py
@@ -1,10 +1,6 @@
-import datetime
-import json
-import subprocess
-from collections import defaultdict
+from datetime import datetime
 
-from kitty.boss import get_boss
-from kitty.fast_data_types import Screen, add_timer
+from kitty.fast_data_types import Screen
 from kitty.tab_bar import (
     DrawData,
     ExtraData,
@@ -12,10 +8,81 @@ from kitty.tab_bar import (
     TabBarData,
     as_rgb,
     draw_attributed_string,
-    draw_tab_with_powerline,
+    draw_title,
 )
+from kitty.utils import color_as_int
 
-timer_id = None
+RIGHT_MARGIN = 1
+
+clock_color = as_rgb(0x7FBBB3)
+utc_color = as_rgb(0x717374)
+
+
+def _draw_left_status(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_title_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+) -> int:
+    if draw_data.leading_spaces:
+        screen.draw(" " * draw_data.leading_spaces)
+
+    draw_title(draw_data, screen, tab, index)
+    trailing_spaces = min(max_title_length - 1, draw_data.trailing_spaces)
+    max_title_length -= trailing_spaces
+    extra = screen.cursor.x - before - max_title_length
+    if extra > 0:
+        screen.cursor.x -= extra + 1
+        screen.draw("…")
+    if trailing_spaces:
+        screen.draw(" " * trailing_spaces)
+    end = screen.cursor.x
+    screen.cursor.bold = screen.cursor.italic = False
+    screen.cursor.fg = 0
+    if not is_last:
+        screen.cursor.bg = as_rgb(color_as_int(draw_data.inactive_bg))
+        screen.draw(draw_data.sep)
+    screen.cursor.bg = 0
+    return end
+
+
+def _draw_right_status(screen: Screen, is_last: bool) -> int:
+    if not is_last:
+        return 0
+
+    draw_attributed_string(Formatter.reset, screen)
+
+    clock = datetime.now().strftime("%H:%M ")
+    utc = datetime.now().strftime("on %A, %B %d, %Y")
+
+    cells = [
+        (clock_color, clock),
+        (utc_color, utc),
+    ]
+
+    right_status_length = RIGHT_MARGIN
+    for cell in cells:
+        right_status_length += len(str(cell[1]))
+
+    draw_spaces = screen.columns - screen.cursor.x - right_status_length
+
+    if draw_spaces > 0:
+        screen.draw(" " * draw_spaces)
+
+    screen.cursor.fg = 0
+    for color, status in cells:
+        screen.cursor.fg = color
+        screen.draw(status)
+    screen.cursor.bg = 0
+
+    if screen.columns - screen.cursor.x > right_status_length:
+        screen.cursor.x = screen.columns - right_status_length
+
+    return screen.cursor.x
 
 
 def draw_tab(
@@ -28,74 +95,20 @@ def draw_tab(
     is_last: bool,
     extra_data: ExtraData,
 ) -> int:
-    global timer_id
 
-    # if timer_id is None:
-    #     timer_id = add_timer(_redraw_tab_bar, 2.0, True)
-    # draw_tab_with_powerline(
-    #     draw_data, screen, tab, before, max_title_length, index, is_last, extra_data
-    # )
-    if is_last:
-        draw_right_status(draw_data, screen)
+    _draw_left_status(
+        draw_data,
+        screen,
+        tab,
+        before,
+        max_title_length,
+        index,
+        is_last,
+        extra_data,
+    )
+    _draw_right_status(
+        screen,
+        is_last,
+    )
+
     return screen.cursor.x
-
-
-def draw_right_status(draw_data: DrawData, screen: Screen) -> None:
-    # The tabs may have left some formats enabled. Disable them now.
-    draw_attributed_string(Formatter.reset, screen)
-    cells = create_cells()
-    # Drop cells that wont fit
-    while True:
-        if not cells:
-            return
-        padding = screen.columns - screen.cursor.x - sum(len(c) + 3 for c in cells)
-        if padding >= 0:
-            break
-        cells = cells[1:]
-
-    if padding:
-        screen.draw(" " * padding)
-
-    tab_bg = as_rgb(int(draw_data.inactive_bg))
-    tab_fg = as_rgb(int(draw_data.inactive_fg))
-    default_bg = as_rgb(int(draw_data.default_bg))
-    for cell in cells:
-        # Draw the separator
-        if cell == cells[0]:
-            screen.cursor.fg = tab_bg
-            screen.draw("")
-        else:
-            screen.cursor.fg = default_bg
-            screen.cursor.bg = tab_bg
-            screen.draw("")
-        screen.cursor.fg = tab_fg
-        screen.cursor.bg = tab_bg
-        screen.draw(f" {cell} ")
-
-
-def create_cells() -> list[str]:
-    now = datetime.datetime.now()
-    return [
-        get_laptop_battery_status(),
-        now.strftime("%A %d %b"),
-        now.strftime("%H:%M"),
-    ]
-
-
-def get_laptop_battery_status():
-    try:
-        output = subprocess.getoutput("acpi -b")
-        if not output:
-            return ""
-        parts = output.split(", ")
-        if len(parts) >= 2:
-            percentage = parts[1].strip()
-            return f" {percentage}"
-    except Exception:
-        pass
-    return ""
-
-
-def _redraw_tab_bar(timer_id):
-    for tm in get_boss().all_tab_managers:
-        tm.mark_tab_bar_dirty()
